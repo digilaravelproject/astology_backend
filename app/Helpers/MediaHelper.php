b16c5c50af1d39baa2513dbff4ce0e1185d7f9bd
shaikhzaid01 <shaikhzaid468@gmail.com>
2026-06-12 17:15:01 +0530

    attachemnt url

--- app/Helpers/MediaHelper.php
+++ app/Helpers/MediaHelper.php
@@ -2,11 +2,32 @@
 
 namespace App\Helpers;
 
-use Illuminate\Support\Facades\Storage;
-
 class MediaHelper
 {
     public static function getUrl(?string $path): ?string
+    {
+        if (empty($path)) {
+            return null;
+        }
+
+        // If it's a full URL, extract the path after /storage/
+        if (filter_var($path, FILTER_VALIDATE_URL)) {
+            $parsed = parse_url($path, PHP_URL_PATH);
+            if ($parsed) {
+                $path = ltrim($parsed, '/');
+            }
+        }
+
+        $path = ltrim($path, '/');
+
+        $path = preg_replace('#^storage/#', '', $path);
+
+        $path = preg_replace('#^public/#', '', $path);
+
+        return $path;
+    }
+
+    public static function getFullUrl(?string $path): ?string
     {
         if (empty($path)) {
             return null;
@@ -22,7 +43,7 @@ class MediaHelper
 
         $path = preg_replace('#^public/#', '', $path);
 
-        $url = Storage::disk('public')->url($path);
+        $url = \Illuminate\Support\Facades\Storage::disk('public')->url($path);
 
         if ($url && !str_starts_with($url, 'http://') && !str_starts_with($url, 'https://') && !str_starts_with($url, '//')) {
             $appUrl = rtrim(config('app.url', 'http://localhost'), '/');
@@ -30,10 +51,5 @@ class MediaHelper
         }
 
         return $url;
-    }
-
-    public static function getFullUrl(?string $path): ?string
-    {
-        return self::getUrl($path);
     }
 }
